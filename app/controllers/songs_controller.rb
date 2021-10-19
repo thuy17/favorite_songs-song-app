f11bc3cffd649759593e600d545454a31fa62406
thuy17 <thuytienguyen17@gmail.com>
2021-10-18 19:18:10 -0500

    add more songs and create requests http

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,27 @@
 class SongsController < ApplicationController
+  def index
+    songs = Song.all
+    render json: songs.as_json
+  end
+
+  def create
+    song = Song.new(
+      title: params[:title],
+      album: params[:album],
+      artist: params[:artist],
+      year: params[:year]
+    )
+    song.save
+  render json: song.as_json
+  end
+
+  def show
+    song = Song.find_by(id: params[:id])
+    render json: song.as_json
+  end
+
+  def update
+    render json: song.as_json
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ #index, show all data
+  get "/songs" => "songs#index"
+
+  #create, create additional data
+  post "/songs" => "songs#create"
+
+  #show, show data by params ID
+  get "/songs/:id" => "songs#show"
+
+  #update data
+  patch "/songs/:id" => "songs#update"
 end
